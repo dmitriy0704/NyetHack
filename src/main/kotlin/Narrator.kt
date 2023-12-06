d06009a06c0045754d7c3f06133aa3c1893b1529
Dmitriy Folomkin <dmitriy.folomkin@gmail.com>
2023-12-06 22:35:33 +0300

    c.170

---
+++ src/main/kotlin/Narrator.kt
@@ -0,0 +1,8 @@
+val narrationModifier: (String) -> String = {
+    val numExclamationPoints = 3
+    it.uppercase() + "!".repeat(numExclamationPoints)
+}
+
+fun narrate(message: String) {
+    println(narrationModifier(message))
+}

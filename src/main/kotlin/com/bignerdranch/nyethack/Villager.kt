cfa8905362688a219a8440c4a4c1025ad4629c05
Dmitriy Folomkin <dmitriy.folomkin@gmail.com>
2023-12-27 22:54:27 +0300

    Наследование...

--- src/main/kotlin/com/bignerdranch/nyethack/Villager.kt
+++ src/main/kotlin/com/bignerdranch/nyethack/Villager.kt
@@ -1,14 +1,16 @@
 package com.bignerdranch.nyethack
 
-class Village(val name: String, val homeTown: String){
+class Villager(val name: String, val homeTown: String){
     val personality: String
     val race = "Dwarf"
     var age = 50
         private set
+
     init {
         println("initializing village")
         personality = "Outgoing"
     }
+
     constructor(name: String) : this(name, "Bavaria"){
         age = 99
     }

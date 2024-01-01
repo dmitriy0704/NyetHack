8a8a369ae4d99c984f3ae524175ce3097a46776c
Dmitriy Folomkin <dmitriy.folomkin@gmail.com>
2024-01-01 13:36:59 +0300

    in и out

--- src/main/kotlin/com/bignerdranch/nyethack/Loot.kt
+++ src/main/kotlin/com/bignerdranch/nyethack/Loot.kt
@@ -1,12 +1,18 @@
 package com.bignerdranch.nyethack
 
-class LootBox<T>(var contents: T) {
+class LootBox<T : Loot>(var contents: T) {
     var isOpen = false
         private set
 
     fun takeLoot(): T? {
         return contents.takeIf { !isOpen }
             .also { isOpen = true }
+    }
+}
+
+class DropOffBox<T> where T : Loot, T : Sellable {
+    fun sellLoot(sellableLoot: T): Int {
+        return (sellableLoot.value * 0.7).toInt()
     }
 }
 

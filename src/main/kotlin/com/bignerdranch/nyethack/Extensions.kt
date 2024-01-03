8ae6cd9dfc38f21d499e4a842b700dc14a0161a9
Dmitriy Folomkin <dmitriy.folomkin@gmail.com>
2024-01-03 20:36:16 +0300

    Расширения - c.402

--- src/main/kotlin/com/bignerdranch/nyethack/Extensions.kt
+++ src/main/kotlin/com/bignerdranch/nyethack/Extensions.kt
@@ -3,7 +3,10 @@ package com.bignerdranch.nyethack
 fun String.addEnthusiasm(enthusiasmLevel: Int = 1) =
     this + "!".repeat(enthusiasmLevel)
 
-fun Any.print(): Any{
+fun <T> T.print(): T {
     println(this)
     return this
 }
+
+operator fun List<List<Room>>.get(coordinate: Coordinate) =
+    getOrNull(coordinate.y)?.getOrNull(coordinate.x)
